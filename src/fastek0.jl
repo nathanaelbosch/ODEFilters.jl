b1408876b618fbdf8d553143d09efeab4825c659
Nathanael Bosch <nathanael.bosch@gmail.com>
2020-12-18 19:27:31 +0100

    Even more optimized FastEK0, exploiting Kronecker structure!

--- src/fastek0.jl
+++ src/fastek0.jl
@@ -52,8 +52,9 @@ function OrdinaryDiffEq.alg_cache(
     @assert alg.prior == :ibm "Only the ibm prior is implemented so far"
     @assert alg.diffusionmodel == :dynamic "FastEK0 only uses dynamic diffusion"
 
-    Precond = preconditioner(d, q)
-    A, Q = ibm(d, q, uEltypeNoUnits)
+    Precond = preconditioner(1, q)
+    _, Q = ibm(1, q, uEltypeNoUnits)
+    A, _ = vanilla_ibm(1, q, uEltypeNoUnits)
     # A, Q = vanilla_ibm(d, q, uEltypeNoUnits)
     # Q = Matrix(Q)
     R = zeros(d, d)
@@ -73,7 +74,8 @@ function OrdinaryDiffEq.alg_cache(
 
     initdiff = one(uEltypeNoUnits)
 
-    I0, I1 = SVector{d}(1:d), SVector{d}(d+1:2d)
+    # I0, I1 = SVector{d}(1:d), SVector{d}(d+1:2d)
+    I0, I1 = 1:d, d+1:2d
 
     return FastEK0ConstantCache{
         typeof(A), typeof(Q),
@@ -98,43 +100,61 @@ function OrdinaryDiffEq.perform_step!(integ, cache::FastEK0ConstantCache, repeat
     tnew = t + dt
 
     # Setup
-    T = Precond(dt); TI = inv(T)
-    Ah, QhL = TI*A*T, TI*Q.L
-    HQhL = @view QhL[I1, :]
-    HQH = HQhL*HQhL'
+    TI = inv(Precond(dt))
+    Ah = A(dt)
+    QhL = TI*Q.L
+    KI = 1:d:D
+    smallAh = Ah
+    smallQhL = QhL
+    HQhL = @view QhL[2, :]
+    HQH = HQhL'HQhL
 
     m, P = x.μ, x.Σ
-    PL = P.squareroot
+    PL = @view P.squareroot[KI, KI]
 
     # Predict - Mean
-    m_p = Ah*m
+    # m_p = Ah*m
+    m_p = vec(reshape(m, (d, q+1)) * smallAh')
+    # @assert m_p == m_p_tricked
     u_pred = @view m_p[I0]  # E0 * m_p
     du_pred = @view m_p[I1]  # E1 * m_p
 
     # Measure
     du = f(u_pred, p, tnew)
-    z = du_pred - du
+    z_neg = du - du_pred
 
     # Calibrate
-    σ² = z' * (HQH \ z) / d  # z'inv(H*Q*H')z / d
+    # σ² = z' * (HQH \ z) / d  # z'inv(H*Q*H')z / d
+    σ² = z_neg'z_neg / HQH
     cache.diffusion = σ²
 
     # Predict - Cov
     QhL_calibrated = sqrt(σ²) * QhL
-    _, PpR = qr([Ah*PL QhL_calibrated]')
+    small_qr_input = [Ah*PL QhL_calibrated]'
+    _, PpR = qr(small_qr_input)
     PpL = PpR'
+    _PpL = [Ah*PL QhL_calibrated]
+    # @info "sizes?" PpL _PpL
+    @assert PpL*PpL' ≈ _PpL*_PpL'
 
     # Measurement Cov
     @assert iszero(R)
-    SL = PpL[I1, :]
-    S = SL*SL'
+    SL = @view PpL[2, :]
+    S = SL'SL
+    _SL = @view _PpL[2, :]
+    _S = _SL'_SL
+    @assert S ≈ _S
 
     # Update
     Sinv = inv(S)
-    K = PpL * (PpL[I1, :])' * Sinv  # P_p * H' * inv(S)
-    m_f = m_p .+ K * (0 .- z)
-    PfL = PpL - K*PpL[I1, :]  # P_f = P_p - K*S*K'
-    P_f = SquarerootMatrix(PfL)
+    K = PpL * PpL[2, :] * Sinv  # P_p * H' * inv(S)
+    _K = _PpL * _PpL[2, :] * inv(_S)  # P_p * H' * inv(S)
+    @assert K ≈ _K
+    m_f = m_p .+ vec((z_neg)*_K')
+    PfL = PpL - _K*PpL[2, :]'  # P_f = P_p - K*S*K'
+    @info "sizes?" PpL _PpL _K*PpL[2, :]' _K*_PpL[2, :]'
+    (t>0) && error()
+    P_f = SquarerootMatrix(kron(PfL, I(d)))
 
     x_filt = Gaussian(m_f, P_f)
     integ.u = m_f[I0]
@@ -142,7 +162,7 @@ function OrdinaryDiffEq.perform_step!(integ, cache::FastEK0ConstantCache, repeat
     # Estimate error for adaptive steps
     if integ.opts.adaptive
 
-        err_est_unscaled = sqrt.(σ².*diag(HQH))
+        err_est_unscaled = sqrt(σ²*HQH)
 
         err = DiffEqBase.calculate_residuals(
             dt * err_est_unscaled, integ.u, integ.uprev,
@@ -230,6 +250,9 @@ function OrdinaryDiffEq.alg_cache(
     @unpack d, q, x = constants
     D = d*(q+1)
 
+    A, Q = ibm(d, q, uEltypeNoUnits)
+    Precond = preconditioner(d, q)
+
     # Create some arrays to cache into
     cov = zeros(uEltypeNoUnits, D, D)
     K = zeros(uEltypeNoUnits, D, D)
@@ -251,16 +274,16 @@ function OrdinaryDiffEq.alg_cache(
 
 
     return FastEK0Cache{
-        typeof(constants.A), typeof(constants.Q),
+        typeof(A), typeof(Q),
         typeof(constants.R), typeof(constants.Proj), typeof(constants.SolProj),
-        typeof(constants.Precond),
+        typeof(Precond),
         typeof(constants.x), typeof(constants.diffusion), typeof(constants.log_likelihood),
         typeof(constants.I0),
         # Mutable stuff
         typeof(u),
         typeof(m_tmp), typeof(P_tmp), typeof(S_tmp), typeof(K_tmp), typeof(preQRmat),
     }(
-        constants.d, constants.q, constants.A, constants.Q, constants.R, constants.Proj, constants.SolProj, constants.Precond,
+        constants.d, constants.q, A, Q, constants.R, constants.Proj, constants.SolProj, Precond,
         constants.x, constants.diffusion,
         constants.I0, constants.I1,
         constants.log_likelihood,

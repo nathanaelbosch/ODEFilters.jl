98621b44a725e9c72e575bf46cde43295541179d
Nathanael Bosch <nathanael.bosch@gmail.com>
2020-12-19 00:21:49 +0100

    faster preconditioning

--- src/preconditioning.jl
+++ src/preconditioning.jl
@@ -5,7 +5,7 @@ function preconditioner(d, q)
         @simd for i in 1:d
             val = h^(-q-1/2)
             @simd for j in 0:q
-                P_preallocated[j*d + i,j*d + i] = h^(j-q-1/2)
+                # P_preallocated[j*d + i,j*d + i] = h^(j-q-1/2)
                 P_preallocated[j*d + i,j*d + i] = val
                 val *= h
             end
@@ -17,15 +17,14 @@ function preconditioner(d, q)
 end
 
 function invpreconditioner(d, q)
+    @assert d == 1
     P_preallocated = Diagonal(zeros(d*(q+1)))
 
-    @fastmath @inbounds function P(h)
-        @simd for i in 1:d
-            val = h^(q+1/2)
-            @simd for j in 0:q
-                P_preallocated[j*d + i,j*d + i] = val
-                val /= h
-            end
+    @fastmath function P(h)
+        val = h^(q + 1/2)
+        @inbounds for j in 1:q+1
+            P_preallocated[j,j] = val
+            val /= h
         end
         return P_preallocated
     end
